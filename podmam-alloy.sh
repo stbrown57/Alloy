41a5905ffa155dbaf9845f5f28b2fecf95c995f7
Steve Brown <stbrown@6browns.org>
2025-09-23 10:23:00 -0400

    Update podmam-alloy.sh
    Added volume for data

--- podmam-alloy.sh
+++ podmam-alloy.sh
@@ -1,5 +1,6 @@
 podman run \
   -v alloy.conf:/etc/alloy:z \
+  -v alloy.data:/var/lib/alloy/data:z \
   -p 12345:12345 \
   docker.io/grafana/alloy:latest \
     run --server.http.listen-addr=0.0.0.0:12345 --storage.path=/var/lib/alloy/data \
